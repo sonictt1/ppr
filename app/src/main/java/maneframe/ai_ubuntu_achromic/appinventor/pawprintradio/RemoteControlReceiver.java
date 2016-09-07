4210b634c9d0e4bde8dddf7a55b0465c6cfd5af5
Scott <wdscott@iastate.edu>
2016-09-07 11:44:13 -0500

    added media session

--- app/src/main/java/maneframe/ai_ubuntu_achromic/appinventor/pawprintradio/RemoteControlReceiver.java
+++ app/src/main/java/maneframe/ai_ubuntu_achromic/appinventor/pawprintradio/RemoteControlReceiver.java
@@ -1,17 +1,32 @@
 package maneframe.ai_ubuntu_achromic.appinventor.pawprintradio;
 
-import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
+import android.view.KeyEvent;
 
 /**
  * Created by Sonic on 8/30/2016.
  */
-public class RemoteControlReceiver extends Service {
-    @Nullable
+public class RemoteControlReceiver extends BroadcastReceiver {
+
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public void onReceive(Context context, Intent intent) {
+        if(Intent.ACTION_MEDIA_BUTTON.equals((intent.getAction())))
+        {
+            KeyEvent event = (KeyEvent) intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+            if(KeyEvent.KEYCODE_MEDIA_PLAY == event.getKeyCode())
+            {
+
+            }
+            else if(KeyEvent.KEYCODE_MEDIA_PAUSE == event.getKeyCode())
+            {
+
+            }
+            else if(KeyEvent.KEYCODE_MEDIA_STOP == event.getKeyCode())
+            {
+
+            }
+        }
     }
 }

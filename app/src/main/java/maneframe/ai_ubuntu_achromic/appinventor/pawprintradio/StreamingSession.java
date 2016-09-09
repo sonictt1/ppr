6308172744099da92b840467389e49033a22246b
Scott <wdscott@iastate.edu>
2016-09-09 11:58:20 -0500

    MediaFocus

--- app/src/main/java/maneframe/ai_ubuntu_achromic/appinventor/pawprintradio/StreamingSession.java
+++ app/src/main/java/maneframe/ai_ubuntu_achromic/appinventor/pawprintradio/StreamingSession.java
@@ -2,11 +2,17 @@ package maneframe.ai_ubuntu_achromic.appinventor.pawprintradio;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.BitmapFactory;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.media.MediaMetadataCompat;
+import android.support.v4.media.session.MediaButtonReceiver;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v4.media.session.PlaybackStateCompat;
 import android.support.v7.app.NotificationCompat;
@@ -24,9 +30,24 @@ public class StreamingSession extends MediaSessionCompat.Callback {
     MediaPlayer player;
     Context mediaContext;
     Notification mediaNotification;
+    AudioManager audioManager;
+
+    private BroadcastReceiver noisyReciever =
+        new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                onPause();
+            }
+        };
 
     public StreamingSession(Context context) {
         super();
+        audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        if(!requestAudioFocus()) {
+            Log.e("StreamingSession", "Audio Focus not granted");
+            return;
+        }
+
         mediaContext = context;
         mediaButtonReceiver = new ComponentName(mediaContext, RemoteControlReceiver.class);
         mediaSession = new MediaSessionCompat(mediaContext, "stream", mediaButtonReceiver, null);
@@ -72,6 +93,7 @@ public class StreamingSession extends MediaSessionCompat.Callback {
         if(!mediaSession.isActive()) mediaSession.setActive(true);
         mediaSession.setPlaybackState(getPlayingPlayState());
         setUpNotification();
+        registerNoisyReceiver();
         player.start();
     }
 
@@ -87,7 +109,9 @@ public class StreamingSession extends MediaSessionCompat.Callback {
         super.onStop();
         if(mediaSession.isActive()) mediaSession.setActive(false);
         if(player.isPlaying()) player.stop();
+        audioManager.abandonAudioFocus(getFocusChangeListener());
         mediaSession.setPlaybackState(getStoppedPlayState());
+        unregisterNoisyReceiver();
         player.release();
     }
 
@@ -146,4 +170,51 @@ public class StreamingSession extends MediaSessionCompat.Callback {
         NotificationManager notifyManager = (NotificationManager) mediaContext.getSystemService(Context.NOTIFICATION_SERVICE);
         notifyManager.notify(mediaContext.getResources().getInteger(R.integer.notification_request_id), mediaNotification);
     }
+
+    private boolean requestAudioFocus() {
+        int requestResult = audioManager.requestAudioFocus(getFocusChangeListener(), AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+
+        return requestResult == AudioManager.AUDIOFOCUS_REQUEST_GRANTED;
+    }
+
+    private AudioManager.OnAudioFocusChangeListener getFocusChangeListener() {
+        return new AudioManager.OnAudioFocusChangeListener() {
+            @Override
+            public void onAudioFocusChange(int i) {
+                switch (i) {
+                    case AudioManager.AUDIOFOCUS_LOSS:
+                        onStop();
+                        break;
+                    case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                        onPause();
+                        break;
+                    case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                        getMediaPlayer().setVolume(.5f, .5f);
+                        break;
+                    case AudioManager.AUDIOFOCUS_GAIN:
+                        getMediaPlayer().setVolume(1.0f, 1.0f);
+                        break;
+                }
+            }
+        };
+    }
+
+    private void registerNoisyReceiver() {
+        IntentFilter filter = new IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY);
+        mediaContext.registerReceiver(noisyReciever, filter);
+    }
+
+    private void unregisterNoisyReceiver() {
+        mediaContext.unregisterReceiver(noisyReciever);
+    }
+
+    private void notifyUser_Play() {
+        NotificationCompat.Builder builder = MediaStyleHelper.from(mediaContext, mediaSession);
+        builder.setSmallIcon(R.drawable.ic_play_arrow_white_24dp).setColor(ContextCompat.getColor(mediaContext, R.color.colorPrimaryDark));
+        builder.addAction(new NotificationCompat.Action(R.drawable.ic_pause_white_24dp, "Pause", MediaButtonReceiver.buildMediaButtonPendingIntent(mediaContext, PlaybackStateCompat.ACTION_PLAY_PAUSE)));
+        builder.setStyle(new NotificationCompat.MediaStyle().setShowActionsInCompactView(0).setMediaSession(mediaSession.getSessionToken()));
+        NotificationManager manager = (NotificationManager) mediaContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(mediaContext.getResources().getInteger(R.integer.notification_request_id), builder.build());
+    }
+
 }
